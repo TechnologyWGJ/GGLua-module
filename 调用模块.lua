7e50b95c427a16ffde44f1a080c18cba90754c3a
Dump <120537913+TechnologyWGJ@users.noreply.github.com>
2023-05-24 13:44:18 +0800

    Update 调用模块.lua

--- 调用模块.lua
+++ 调用模块.lua
@@ -1,4 +1,4 @@
--- Ps: require函数的参数可以是绝对路径，也可以是相对路径
+-- Ps: require函数的参数可以是绝对路径，也可以是相对路径，模块的后缀名是.lua，但在加载它时，我们不能加上后缀
 mymodule = require("模块") --使用require函数加载模块，他会返回一个Table,我们使用一个变量来接收
 r1 = mymodule.add(1,2) --使用变量调用函数
 r2 = mymodule.sub(5,1) --使用变量调用函数
